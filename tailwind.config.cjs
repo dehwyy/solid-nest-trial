604fdd17df4ee9d934eaf5e44be883dab2e24102
dehwyy <91pony@mail.ru>
2023-01-30 03:41:43 +0300

    update readme and cfgs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,22 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        slide: 'slide 3s ease-in-out infinite',
-      },
-      keyframes: {
-        slide: {
-          '0%': { transform: 'translateX(-500px)' },
-          '100%': { transform: 'translateX(500px)' },
-        }
-      }
-    },
-    fontFamily: {
+    extend: {}, fontFamily: {
       "poppins": ['Poppins', 'sans-serif'],
       "inter": ['Inter', 'sans-serif'],
       "japanese": ['Noto Sans JP', 'sans-serif'],
-    }
-  },
-  plugins: []
-};
+    }}, plugins: []
+}
