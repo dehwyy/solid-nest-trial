2f505a1f04def1fa2ab75d6beaa896f0e64a5688
dehwyy <91pony@mail.ru>
2023-02-01 04:27:37 +0300

    enabled cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import setupSwagger from '../config/swagger';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import setupSwagger from "../config/swagger"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
-  await app.listen(process.env.PORT);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  setupSwagger(app)
+  await app.listen(process.env.PORT)
 }
 
-bootstrap().then(() => console.log(`http://localhost:${process.env.PORT}`));
+bootstrap().then(() => console.log(`http://localhost:${process.env.PORT}`))
